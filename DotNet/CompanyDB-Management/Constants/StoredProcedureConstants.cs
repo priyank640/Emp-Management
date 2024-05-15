98d89dee30b040c9a28edbb754cef2abf26cf79f
priyank640 <63116500+priyank640@users.noreply.github.com>
2024-05-16 00:55:34 +0530

    Create StoredProcedureConstants.cs

---
+++ DotNet/CompanyDB-Management/Constants/StoredProcedureConstants.cs
@@ -0,0 +1,21 @@
+namespace CompanyDB_Management.Constants
+{
+    public class StoredProcedureConstants
+    {
+        public class Employee
+        {
+            public const string GetAllEmployees = "GetAllEmployees";
+
+            public const string AddEmployees = "AddEmployee";
+
+            public const string UpdateEmployee = "UpdateEmployee";
+
+            public const string DeleteEmployee = "DeleteEmployee";
+
+
+
+        }
+
+
+    }
+}
